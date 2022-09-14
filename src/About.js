fb2c56bd1e6ed09721672543a6b864a4c4902f15
hazedrops <hazedrops@hotmail.com>
2022-09-13 22:55:41 -0700

    Higher Order Component Example 1

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Rainbow from './Rainbow'
+
+function About() {
+  return (
+    <div className='container'>
+      <h4 className='center'>About</h4>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
+        accusamus eius modi blanditiis consectetur perspiciatis
+      </p>
+    </div>
+  )
+}
+
+export default Rainbow(About)
